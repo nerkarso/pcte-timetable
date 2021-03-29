59e4b8ad94da27cf3956d248638d68ee13c056d1
Ner Karso <nerpkarso@gmail.com>
2021-03-29 20:10:59 +0530

    Update choose classname button

--- src/components/ChooseClassName.js
+++ src/components/ChooseClassName.js
@@ -1,45 +1,45 @@
+import { ClassNameContext } from 'context/classname-context';
+import Loading from 'elements/Loading';
+import Modal from 'elements/Modal';
+import useLatestTimetable from 'hooks/useLatestTimetable';
+import useModal from 'hooks/useModal';
 import React, { useContext } from 'react';
-import { ClassNameContext } from '../context/classname-context';
-import { classNames } from '../data/db.json';
-import Modal from '../elements/Modal';
-import useModal from '../hooks/useModal';
-import '../styles/Button.scss';
-import '../styles/List.scss';
+import { useHistory } from 'react-router-dom';
+import 'styles/Button.scss';
+import 'styles/List.scss';
 
 export default function ChooseClassName({ placeholder }) {
+  const history = useHistory();
+  const { data, error, loading } = useLatestTimetable();
   const { isModalShown, toggleModalShown } = useModal();
   const { className, changeClassName } = useContext(ClassNameContext);
 
-  const handleItemClick = name => {
+  const handleItemClick = (name) => {
     changeClassName(name);
-    toggleModalShown();
+    history.push('/');
   };
+
+  if (loading) return <Loading />;
+  if (data.error || error) return null;
+  if (!data.classnames) return null;
 
   return (
     <>
-      <button
-        className="button--chooser"
-        title="Choose your class"
-        onClick={toggleModalShown}
-      >
+      <button className="button--chooser" title="Choose your class" onClick={toggleModalShown}>
         {className ? className : placeholder}
       </button>
       <Modal
         headerTitle="Choose your class"
         closeTitle="Close"
         isModalShown={isModalShown}
-        toggleModalShown={toggleModalShown}
-      >
+        toggleModalShown={toggleModalShown}>
         <div className="list">
-          {classNames.map(name => (
+          {data.classnames.map((item) => (
             <button
-              className={`list__item${
-                className === name ? ' list__item--active' : ''
-              }`}
-              onClick={() => handleItemClick(name)}
-              key={name}
-            >
-              {name}
+              className={`list__item${className === item.name ? ' list__item--active' : ''}`}
+              onClick={() => handleItemClick(item.name)}
+              key={item._id}>
+              {item.name}
             </button>
           ))}
         </div>
