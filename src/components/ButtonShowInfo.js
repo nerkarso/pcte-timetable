015e06a6cf2d4c5cc57ce87851223146d9e7155e
Ner Karso <nerpkarso@gmail.com>
2021-03-29 20:10:55 +0530

    Update info button

--- src/components/ButtonShowInfo.js
+++ src/components/ButtonShowInfo.js
@@ -1,9 +1,11 @@
+import Button from 'elements/Button';
+import Loading from 'elements/Loading';
+import Modal from 'elements/Modal';
+import useLatestTimetable from 'hooks/useLatestTimetable';
+import useModal from 'hooks/useModal';
 import React from 'react';
 import { Info } from 'react-feather';
-import { date } from '../data/db.json';
-import Button from '../elements/Button';
-import Modal from '../elements/Modal';
-import useModal from '../hooks/useModal';
+import 'styles/List.scss';
 
 export default function ButtonShowInfo() {
   const { isModalShown, toggleModalShown } = useModal();
@@ -13,15 +15,28 @@ export default function ButtonShowInfo() {
       <Button title="Show info" onClick={toggleModalShown}>
         <Info color="var(--text)" size={24} />
       </Button>
-      <Modal
-        headerTitle="Info"
-        closeTitle="Close"
-        isModalShown={isModalShown}
-        toggleModalShown={toggleModalShown}
-      >
-        <p>Date: {date}</p>
-        <p>Version: {process.env.REACT_APP_VERSION}</p>
+      <Modal headerTitle="Info" closeTitle="Close" isModalShown={isModalShown} toggleModalShown={toggleModalShown}>
+        <div className="list">
+          <p className="list__item__between">
+            <span>Date</span>
+            <LatestDate />
+          </p>
+          <p className="list__item__between">
+            <span>Version</span>
+            <b>{process.env.REACT_APP_VERSION}</b>
+          </p>
+        </div>
       </Modal>
     </>
   );
+}
+
+function LatestDate() {
+  const { data, error, loading } = useLatestTimetable();
+
+  if (loading) return <Loading />;
+  if (data.error || error) return <i>Error</i>;
+  if (!data.date) return <i>None</i>;
+
+  return <b>{data.date}</b>;
 }
