0d4eff8b3a5c214204ad638a3e9e249cfd64f65b
Ner Karso <nerpkarso@gmail.com>
2021-04-27 06:35:44 +0530

    Update lecture loading skeleton

--- src/components/DaySkeleton.js
+++ src/components/DaySkeleton.js
@@ -4,8 +4,17 @@ export default function DaySkeleton() {
   return (
     <article className="day">
       <div className="day__container">
-        <h1 className="day__heading">Loading</h1>
+        <h1 className="day__heading day__heading--skeleton">Day</h1>
+        <div className="day__grid">
+          {[1, 2, 3].map((i) => (
+            <LectureSkeleton key={i} />
+          ))}
+        </div>
       </div>
     </article>
   );
+}
+
+function LectureSkeleton() {
+  return <section className="lecture lecture--skeleton"></section>;
 }
